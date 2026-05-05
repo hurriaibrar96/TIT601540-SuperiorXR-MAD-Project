347ecf9c328339f03a94fff3a439b81bb584b9a9
Hurria Ibrar <hurriaibrar96@gmail.com>
2026-05-05 02:33:43 -0700

    Lab 11 Integration of APIs

---
+++ lib/user_model.dart
@@ -0,0 +1,22 @@
+import 'dart:convert';
+
+List<UserModel> userModelFromJson(String str) =>
+    List<UserModel>.from(json.decode(str).map((x) => UserModel.fromJson(x)));
+
+class UserModel {
+  int id;
+  String name;
+  String email;
+
+  UserModel({
+    required this.id,
+    required this.name,
+    required this.email,
+  });
+
+  factory UserModel.fromJson(Map<String, dynamic> json) => UserModel(
+        id: json["id"],
+        name: json["name"],
+        email: json["email"],
+      );
+}
