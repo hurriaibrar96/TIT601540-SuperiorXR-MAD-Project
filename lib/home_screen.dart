7a9bfd42ea368f70b1387e48ea6d1d8d5ef0616f
Hurria Ibrar <hurriaibrar96@gmail.com>
2026-05-05 02:36:20 -0700

    API's of home screen for lab 11
    Implement HomeScreen to fetch and display users from API.

---
+++ lib/home_screen.dart
@@ -0,0 +1,50 @@
+import '../api/api_service.dart';
+import '../models/user_model.dart';
+
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  List<UserModel> users = [];
+  bool isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    fetchUsers();
+  }
+
+  void fetchUsers() async {
+    try {
+      users = await ApiService.getUsers(); // ✅ API CALLED HERE
+    } catch (e) {
+      print(e);
+    }
+
+    setState(() => isLoading = false);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Users API')),
+      body: isLoading
+          ? const Center(child: CircularProgressIndicator())
+          : ListView.builder(
+              itemCount: users.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(users[index].name),
+                  subtitle: Text(users[index].email),
+                );
+              },
+            ),
+    );
+  }
+}
