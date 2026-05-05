bff65ccb9e3bef0e77c04b8a56e0553a04787daa
Javeria Nawal <263147789+Javeria-Nawal049@users.noreply.github.com>
2026-05-05 23:35:36 +0500

    Update main.dart
    Added lab 12 main.dart code

--- lib/main.dart
+++ lib/main.dart
@@ -1287,6 +1287,103 @@ class ProfileScreen extends StatelessWidget {
         ),
       ),
     );
+    \\MAD LAB 12
+      \\CONNECTING FIREBASE TO MOBILE APP
+      \\BY Javeria Nawal
+      import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: const FirebaseOptions(
+      apiKey: "AIzaSyD4eJqO9hHbFtVa5SsAXRJvL4c0eAf7JXY",
+      appId: "1:832561075061:web:b9e95878976d368ad1f93c",
+      messagingSenderId: "832561075061",
+      projectId: "lab12app-9e84a",
+      authDomain: "lab12app-9e84a.firebaseapp.com",
+      storageBucket: "lab12app-9e84a.firebasestorage.app",
+    ),
+  );
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext c) {
+    return const MaterialApp(
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  final db = FirebaseFirestore.instance;
+  final con = TextEditingController();
+
+  void addPlace() async {
+    if (con.text.isEmpty) return;
+
+    await db.collection("places").add({
+      "name": con.text,
+    });
+
+    con.clear();
+  }
+
+  @override
+  Widget build(BuildContext c) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Campus Guide App")),
+      body: Column(
+        children: [
+          TextField(
+            controller: con,
+            decoration: const InputDecoration(
+              hintText: "Enter place (Library, Cafe...)",
+            ),
+          ),
+          ElevatedButton(
+            onPressed: addPlace,
+            child: const Text("Add Place"),
+          ),
+          Expanded(
+            child: StreamBuilder(
+              stream: db.collection("places").snapshots(),
+              builder: (c, s) {
+                if (!s.hasData) return const Text("Loading...");
+
+                var docs = s.data!.docs;
+
+                return ListView.builder(
+                  itemCount: docs.length,
+                  itemBuilder: (c, i) {
+                    return ListTile(
+                      title: Text(docs[i]["name"]),
+                    );
+                  },
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
   }
 }
 
